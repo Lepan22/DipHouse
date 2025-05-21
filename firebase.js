4479eeb288f62a748913eccccb6c2c4d281c8887
Lepan22 <contato@lepanpan.com.br>
2025-05-20 21:12:24 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,27 +1,23 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import {
-  getDatabase, ref, push, update, remove, onValue
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
-import {
-  getStorage, ref as sRef, uploadBytes, getDownloadURL, deleteObject
-} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAa_IPmOWZ4-KpWIC7huhhl7mYjYfEJhDk",
   authDomain: "diphouse-control.firebaseapp.com",
   databaseURL: "https://diphouse-control-default-rtdb.firebaseio.com",
   projectId: "diphouse-control",
-  storageBucket: "diphouse-control.appspot.com",
+  storageBucket: "diphouse-control.firebasestorage.app",
   messagingSenderId: "126387069829",
-  appId: "1:126387069829:web:7e365252a98da884075aec"
+  appId: "1:126387069829:web:7e365252a98da884075aec",
+  measurementId: "G-TFRB580278"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
 const storage = getStorage(app);
 
-export {
-  db, ref, push, update, remove, onValue,
-  storage, sRef, uploadBytes, getDownloadURL, deleteObject
-};
+export { app, analytics, database, storage };
