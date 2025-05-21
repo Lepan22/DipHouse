47ee7d64643ee5d6d58e7a2de41c870a9b1a838e
Lepan22 <contato@lepanpan.com.br>
2025-05-20 21:19:16 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,18 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAa_IPmOWZ4-KpWIC7huhhl7mYjYfEJhDk",
-  authDomain: "diphouse-control.firebaseapp.com",
-  databaseURL: "https://diphouse-control-default-rtdb.firebaseio.com",
-  projectId: "diphouse-control",
-  storageBucket: "diphouse-control.firebasestorage.app",
-  messagingSenderId: "126387069829",
-  appId: "1:126387069829:web:7e365252a98da884075aec",
-  measurementId: "G-TFRB580278"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app);
-const storage = getStorage(app);
-
-export { app, analytics, database, storage };
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-storage.js"></script>
+<script>
+  var firebaseConfig = {
+    apiKey: "AIzaSyAa_IPmOWZ4-KpWIC7huhhl7mYjYfEJhDk",
+    authDomain: "diphouse-control.firebaseapp.com",
+    databaseURL: "https://diphouse-control-default-rtdb.firebaseio.com",
+    projectId: "diphouse-control",
+    storageBucket: "diphouse-control.firebasestorage.app",
+    messagingSenderId: "126387069829",
+    appId: "1:126387069829:web:7e365252a98da884075aec",
+    measurementId: "G-TFRB580278"
+  };
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+  var storage = firebase.storage();
+</script>
